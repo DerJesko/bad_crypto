531875adf78e7dc9903fce91a8e0cb605ae73916
Jesko Dujmovic <jeskod+git@gmail.com>
2019-02-06 21:45:12 +0100

    adding finite fields and starting to work on matrices

--- src/fields.rs
+++ src/fields.rs
@@ -1,67 +1,79 @@
 use crate::prime;
 use num_bigint::BigUint;
-use num_traits::{Zero,One};
+use num_traits::{One, Zero};
 use rand::prelude::ThreadRng;
+use std::rc::Rc;
 
-struct FiniteField {
+#[derive(PartialEq, Debug)]
+pub struct FiniteField {
     order: BigUint,
 }
 
 impl FiniteField {
-    pub(crate) fn rand_new(sec_param: usize, rng: &mut ThreadRng) -> Self {
+    pub fn rand_new(sec_param: usize, rng: &mut ThreadRng) -> Self {
         FiniteField {
             order: prime::random_prime(sec_param, rng),
         }
     }
 }
 
-struct FiniteFieldElement {
+#[derive(Debug)]
+pub struct FiniteFieldElement {
     number: BigUint,
-    field: Rc<FiniteField>
+    field: Rc<FiniteField>,
 }
 
 impl FiniteFieldElement {
-    pub(crate) fn zero(g: &FiniteField) -> Self {
+    pub fn zero(g: &Rc<FiniteField>) -> Self {
         FiniteFieldElement {
             number: Zero::zero(),
-            field: g.clone()
+            field: g.clone(),
         }
     }
 
-    pub(crate) fn one(g: &FiniteField) -> Self {
+    pub fn one(g: &Rc<FiniteField>) -> Self {
         FiniteFieldElement {
             number: One::one(),
-            field: g.clone()
+            field: g.clone(),
         }
     }
 
-    pub(crate) fn add(&self, b: &Self) -> Self {
+    pub fn add(&self, b: &Self) -> Self {
         if self.field != b.field {
-            panic!("adding {:?} and {:?} did't work they have differnt fields", self, b);
+            panic!(
+                "adding {:?} and {:?} did't work they have differnt fields",
+                self, b
+            );
         }
         FiniteFieldElement {
             number: &(&self.number + &b.number) % &self.field.order,
-            field: self.field.clone()
+            field: self.field.clone(),
         }
     }
 
-    pub(crate) fn mult(&self, b: &Self) -> Self {
+    pub fn mult(&self, b: &Self) -> Self {
         if self.field != b.field {
-            panic!("multiplying {:?} and {:?} did't work they have differnt fields", self, b);
+            panic!(
+                "multiplying {:?} and {:?} did't work they have differnt fields",
+                self, b
+            );
         }
         FiniteFieldElement {
             number: &(&self.number * &b.number) % &self.field.order,
-            field: self.field.clone()
+            field: self.field.clone(),
         }
     }
 
-    pub(crate) fn pow(&self, b: &Self) -> Self {
+    pub fn pow(&self, b: &Self) -> Self {
         if self.field != b.field {
-            panic!("raising {:?} to the power of {:?} did't work they have differnt fields", self, b);
+            panic!(
+                "raising {:?} to the power of {:?} did't work they have differnt fields",
+                self, b
+            );
         }
         FiniteFieldElement {
             number: self.number.modpow(&b.number, &self.field.order),
-            field: self.field.clone()
+            field: self.field.clone(),
         }
     }
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,19 @@
-use ndarray;
+use crate::fields::FiniteFieldElement;
+use ndarray::Array2;
+use num_bigint::{BigUint, RandBigInt};
 
-struct Matrix();
+pub struct Matrix(pub Array2<FiniteFieldElement>);
+
+impl Matrix {
+    pub fn add(&self, b: &Matrix) -> Matrix {
+        let Matrix(x) = self;
+        let Matrix(y) = b;
+        //assert!(x.len_of() == y.len_of());
+        //assert!(x.len_of() == y.len_of());
+        panic!();
+    }
+
+    pub fn mult(&self, b: &Matrix) -> Matrix {
+        panic!();
+    }
+}
