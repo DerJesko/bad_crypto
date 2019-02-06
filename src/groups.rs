d21c18f2f5952b3a2cbca64e19d87c0430f461ee
Jesko Dujmovic <jeskod+git@gmail.com>
2019-02-06 13:45:09 +0100

    making dlog harder

--- src/groups.rs
+++ src/groups.rs
@@ -8,17 +8,27 @@ use std::rc::Rc;
 #[derive(Debug, Clone, PartialEq)]
 pub struct PrimeGroup {
     pub modulus: BigUint,
+    pub small_prime: u8,
+    pub big_prime: BigUint,
 }
 
 impl PrimeGroup {
     pub fn rand_new(sec_param: usize, rng: &mut ThreadRng) -> Self {
-        PrimeGroup {
-            modulus: prime::random_prime(sec_param, rng),
+        loop {
+            let p = prime::random_prime(sec_param, rng);
+            for i in [2,3,5,7]{
+            if prime::prime_eh(&(p * i)+1) {
+                return PrimeGroup {
+                    modulus: p * i +1,
+                    big_prime: p,
+                    small_prime: i
+                }
+            }}
         }
     }
 
     pub fn exp_inverse(&self, x: &BigUint) -> BigUint {
-        &self.modulus - &(x % &self.modulus)
+        &self.big_prime - &(x % &self.big_prime)
     }
 }
 
@@ -30,20 +40,19 @@ pub struct PrimeGroupElement {
 
 impl PrimeGroupElement {
     pub fn rand_generator(group: Rc<PrimeGroup>, rng: &mut ThreadRng) -> PrimeGroupElement {
-        PrimeGroupElement {
-            number: rng.gen_biguint_range(&TWO(), &group.modulus),
-            group: group.clone(),
-        }
+        let r = rng.gen_biguint_range(&TWO, &group.big_prime);
+        Self::new(r, group)
     }
+
     pub fn new(number: BigUint, group: Rc<PrimeGroup>) -> PrimeGroupElement {
         PrimeGroupElement {
-            number: number,
+            number: number*g.small_prime,
             group: group.clone(),
         }
     }
     pub fn one(g: &Rc<PrimeGroup>) -> Self {
         PrimeGroupElement {
-            number: Zero::zero(),
+            number: One::one()*g.small_prime,
             group: g.clone(),
         }
     }
@@ -56,29 +65,15 @@ impl PrimeGroupElement {
             );
         }
         PrimeGroupElement {
-            number: &(&self.number + &b.number) % &self.group.modulus,
+            number: &(&self.number * &b.number) % &self.group.modulus,
             group: self.group.clone(),
         }
     }
 
     pub fn pow(&self, b: &BigUint) -> Self {
         PrimeGroupElement {
-            number: &(&self.number * b) % &self.group.modulus,
+            number: self.modpow(b, &group.modulus),
             group: self.group.clone(),
         }
     }
 }
-/*
-#[derive(Debug, Clone)]
-pub(crate) struct DoublePrimeGroup {
-    pub(crate) modulus: BigUint,
-}
-
-impl DoublePrimeGroup {
-    pub(crate) fn rand_new(sec_param: usize, rng: &mut ThreadRng) -> (Self, BigUint, BigUint) {
-        let q = prime::random_prime(sec_param, rng);
-        let p = prime::random_prime(sec_param, rng);
-        (DoublePrimeGroup { modulus: &p * &q }, p, q)
-    }
-}
-*/
